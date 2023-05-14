56de16f01bd3218250ef036fce57c26a743c0896
Parisa-Dayani <83198050+Parisa-Dayani@users.noreply.github.com>
2023-05-14 11:51:31 +0330

    Create script_to_backup_files.sh

---
+++ script_to_backup_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#backup each file or directory that have been modified within the last 24 hour
+
+BACKUPFILE=backup-$(date +%m-%d-%Y)
+archive=${1:-$BACKUPFILE}
+
+find . -mtime -1 -type f -print0 | xargs -0 tar rvf "$archive.tar"
+echo "Directory $PWD backed up in archive file \"$archive.tar.gz\"."
+exit 0
